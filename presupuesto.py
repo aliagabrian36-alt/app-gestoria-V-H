8c60d4e447b8bf8234d673af68b2343e4e8fb7f7
aliagabrian36-alt <aliagabrian36@gmail.com>
2026-02-12 15:31:58 -0300

    presupuesto.py

--- presupuesto.py
+++ presupuesto.py
@@ -148,7 +148,7 @@ with col_t1:
         "Informe de Dominio": 10000.0
     }
     tipo_tramite = st.selectbox("Seleccione el Trámite", list(tramites_precios.keys()))
-    valor_tabla = st.number_input("Precio de tabla automotor ($)", min_value=0.0, step=1000.0)
+    valor_tabla = st.number_input("Precio de tabla automotor ($)", min_value=0.0, step=100.0)
 
 with col_t2:
     distancia = st.number_input("Distancia (KM ida y vuelta)", min_value=0.0)
@@ -271,6 +271,7 @@ if st.button("🔄 Actualizar Historial desde la Nube"):
     else:
 
         st.info("No hay registros en la base de datos todavía.")
+
 
 
 
